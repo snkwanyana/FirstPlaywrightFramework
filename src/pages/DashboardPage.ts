8976d1bb586f75ee7885f83352c2d0cdf420ed04
Administrator <sibusiso.nkwanyana@simplify.africa>
2026-08-30 21:44:07 +0200

    improved the element locator for login validation in the Dashboard.ts class

--- src/pages/DashboardPage.ts
+++ src/pages/DashboardPage.ts
@@ -3,7 +3,8 @@ import {ReusableFunctions} from "../utils/reusableFunctions";
 export class DashboardPage extends ReusableFunctions { 
 
     async verifyDashboardPageIsDisplayed() {
-        return await this.isElementVisible(this.page.locator("h2"));
+        // return await this.isElementVisible(this.page.locator("h2"));
+        return await this.isElementVisible(this.page.getByRole('heading', { name: 'Welcome back, admin 👋' }));
         
     }
 }
